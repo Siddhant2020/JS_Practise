87cfac623688b59e3538b19633b4ffeb92cce2e5
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-10 10:53:56 +0000

    Conversion of DataTypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = undefined;
+
+// console.log(typeof score);
+
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN 'Not a Number' type Number
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = "Siddhant";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "Siddhant" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
