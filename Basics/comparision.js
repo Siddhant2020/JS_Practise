b1b426d18b4ba327444f98f5321742bd4179adc4
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-10 12:57:04 +0000

    comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,18 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log("true" == true)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//===  strict check [value check with datatype]
+console.log("2" === 2)
+
+//== value check
+console.log("2" == 2)
--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -22,9 +22,43 @@
 // "" => false
 // "Siddhant" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************************ Operations ***************************
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // 2 raise to the power 3
+// console.log(6/3)
+// console.log(12%8)
+
+// let str1 = "hello"
+// let str2 = " Siddhant"
+// let str3 = str1 + str2
+// console.log(str3)
 
 
+// console.log("1" + 2);
+// console.log("1" + 2 + 3);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log(1 + 3 +"2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
