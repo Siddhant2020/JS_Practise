f5eefae65c4cffe02f3c89af0e5a6b69687e5e30
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-10 06:59:08 +0000

    datatypes explaination

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -2,3 +2,21 @@
 
 // alert(3+3); // we are using nodejs, not browser
 
+let name = "siddhant"
+let age = 18
+let isLoggedin = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value "representation for empty value"
+// undefined => variable is declared but value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof name); //object
