59484cb1268207e292fbceadf211b46319d4ab27
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-11 06:55:11 +0000

    stack and heap memory manage

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -29,20 +29,42 @@ const bigNumber = 3423111437474743938287379437394n // postfix n to make it bigIn
 // console.log(typeof name); //object
 
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 
 
-let obj = {
-    name : "Siddhant",
-    currentAge : 28
+// let obj = {
+//     name : "Siddhant",
+//     currentAge : 28
+// }
+
+// console.log(obj)
+
+// const myfunction = function (){
+//     console.log('Hello world')
+// }
+
+// myfunction()
+
+// console.log(typeof myfunction)
+
+//********************************** Memories ********************************//
+
+/**********Stack memory(Primitive Types), Heap memory(Non-Primitive Types) ********/
+
+let myYoutubeName = 'siddhant.ashokdotcom'
+
+let anotherName = myYoutubeName
+anotherName = 'siddhant'
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let user1 = {
+    email : "user@google.com",
+    upi : 'user@ybl'
 }
 
-console.log(obj)
+let user2 = user1;
+user2.email = "user@gmail.com"
 
-const myfunction = function (){
-    console.log('Hello world')
-}
-
-myfunction()
-
-console.log(typeof myfunction)
+console.log(user1)
+console.log(user2)
