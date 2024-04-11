90a09d5293b2507f7c30e52626b666d631aca87c
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-11 06:43:37 +0000

    dataType Summary

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -6,6 +6,13 @@ let name = "siddhant"
 let age = 18
 let isLoggedin = true
 
+let accountId = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(accountId === anotherId)
+
+const bigNumber = 3423111437474743938287379437394n // postfix n to make it bigInt
+
 // number => 2 to power 53
 // bigint
 // string => ""
@@ -17,6 +24,25 @@ let isLoggedin = true
 
 // object
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); //object
-console.log(typeof name); //object
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+// console.log(typeof name); //object
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+
+let obj = {
+    name : "Siddhant",
+    currentAge : 28
+}
+
+console.log(obj)
+
+const myfunction = function (){
+    console.log('Hello world')
+}
+
+myfunction()
+
+console.log(typeof myfunction)
