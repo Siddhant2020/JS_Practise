fac1c75863e9e20b7618a553066645784089822f
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-11 10:15:39 +0000

    Learning Strings

---
+++ Basics/strings.js
@@ -0,0 +1,16 @@
+const name = "Siddhant"
+const age = 28
+
+const gameName = new String("Siddhant");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const splitString = gameName.split('h');
+console.log(splitString);
+console.log(gameName.replace('hant', ''));
