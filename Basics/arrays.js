01a437aedf79682b9a1a3f75525c219194c954c6
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-12 09:08:00 +0000

    array Part 1

---
+++ Basics/arrays.js
@@ -0,0 +1,26 @@
+// // array
+
+// const myArr = [0,1,2,3,4,5];
+// const myHearos = ["shatiMan", "nagraj"]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[2])
+
+// // Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// console.log(myArr)
+// console.log(typeof myArr)
+
+// myArr.pop();
+// console.log(myArr)
+
+// myArr.unshift(9);
+// console.log(myArr)
+
+// myArr.shift();
+// console.log(myArr)
+// slice doesn't change the original array , splice changes the original array
+
+console.log()
