93804507bef588b0549de759a0d92fd3a6c998a7
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-26 15:07:34 +0000

    control flow and truthy/falsy value

--- ControlFlow/one.js
+++ ControlFlow/one.js
@@ -20,3 +20,48 @@
 //     console.log(" === Compare value with type"); //it doesn't
 // }
 
+
+// const score = 200;
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }else{
+//     const power = "High Jump"
+//     console.log(`User power: ${power}`)
+// }
+
+const balance = 1000;
+// if (balance > 500) console.log('balance is less than 500');
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+
+//     case 2:
+//         console.log("February")
+//         break;
+
+//     case 3:
+//         console.log("March")
+//         break;
+
+//     case 4:
+//         console.log("April")
+//         break;
+        
+//     default:
+//         console.log("default")
+//         break;
+// }
---
+++ ControlFlow/truthy.js
@@ -0,0 +1,47 @@
+// const userEmail = "sid@gmail.com"
+// const userEmail = ""
+// const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got user email")
+// }else{
+//     console.log("Don't have user email");
+// }
+
+//  falsy values
+//false, 0 , "", -0, BigInt 0n, null, undefined, NaN
+
+//truthy values
+//true, 1, "filled", "0", 'false', " ", {}, [], function(){}
+
+// if (userEmail.length) {
+//     console.log("array is filled");
+// }else{
+//     console.log("array is empty");
+//}
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length) {
+//     console.log("Object is not null");
+// }else{
+//     console.log("Object is null");
+// }
+
+//  Nullish Coalescing Operator (??):  to avoid null undefined values
+
+// let val1;
+// val1 = 5 ?? 10;
+
+// console.log(val1);
+
+// let val1 = {}
+// let val1 = null
+// let val2 = val1 ?? {username : "siddhant"};
+
+// console.log(val2);
+
+let val1;
+val1 = null ?? 10 ?? 10
+
+console.log(val1);
