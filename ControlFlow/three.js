c4655944b7e3f412a8452c255645e2eb433f0011
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-27 11:45:21 +0000

    high order array loops

---
+++ ControlFlow/three.js
@@ -0,0 +1,63 @@
+// for of
+// ["","",""]
+// [{},{},{}]
+
+// const arr = [1,2,3,4,5]
+
+// for (const number of arr) {
+//     console.log(number);
+// }
+
+// const myObject = {
+//     hero1: "Spiderman",
+//     hero2: "Ironman",
+//     hero3: "Doctor Strange"
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} : ${myObject[key]}`);
+// }
+
+// const coding = ["js"]
+
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+// coding.forEach(code => {
+//     console.log(code);
+// });
+
+
+// coding.forEach((item, index) => {
+//     console.log(`${item} ${index}`);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// });
+
+// const myCoding = [
+//     {
+//         languageName:"C#",
+//         languageFileName:"cs"
+//     },
+//     {
+//         languageName:"JavaScript",
+//         languageFileName:"js"
+//     },
+//     {
+//         languageName:"Python",
+//         languageFileName:"py"
+//     }
+// ]
+
+// myCoding.forEach((item) => {
+//     console.log(item.languageFileName, item.languageName);
+// });
+
+
