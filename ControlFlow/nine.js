6382e43429a3f3c0747ec98faf0489c01808149c
Siddhant Ashok <71391930+Siddhant2020@users.noreply.github.com>
2024-04-27 12:20:04 +0000

    reduce with implicit return

--- ControlFlow/nine.js
+++ ControlFlow/nine.js
@@ -1,8 +1,10 @@
-const numbers = [1,2,3]
+// const numbers = [1,2,3]
 
-const total = numbers.reduce((acc, curval) => {
-    console.log(`${acc} ${curval}`)
-    return acc + curval;
-}, 2);
+// const total = numbers.reduce((acc, curval) => {
+//     console.log(`${acc} ${curval}`)
+//     return acc + curval;
+// }, 2);
 
-console.log(total);
+// const total = numbers.reduce((acc, curval) => acc+curval , 0);
+
+// console.log(total);
